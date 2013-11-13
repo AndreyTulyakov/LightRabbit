ce4fcdd3c4682a7979d88c057f0bc7c09763c1fc
Andrey Tulaykov <mhyhre@gmail.com>
2013-11-13 21:34:55 +0400

    updated readme

--- src/mhyhre/lightrabbit/game/Levels/DialogBase.java
+++ src/mhyhre/lightrabbit/game/Levels/DialogBase.java
@@ -21,101 +21,92 @@ import android.util.Log;
 
 public class DialogBase {
 
-	private static final String CHARACTER_BASE_FILENAME = "CharacterBase";
-	private static final String ID = "id";
-	private static final String ACTOR_ID = "actor_id";
-	private static final String NEXT_ID = "next_id";
-	private static final String DIALOG = "dialog";
-	private static final String DIALOG_CAPTION = "caption";
-	private static final String DIALOG_IS_FAST = "fast";
-	private static final String REPLIC = "replic";
-	
-	Map<Integer, Dialog> dialogs;
-	private String characterBaseFilename;
-	
-	public DialogBase(String filename) {
-		
-		dialogs = new HashMap<Integer, Dialog>();
-		
-		try {
-			InputStream is = MainActivity.Me.getAssets().open(MainActivity.LEVELS_FOLDER + filename);
-			
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = dbf.newDocumentBuilder();
-			Document doc = docBuilder.parse(is);
+    private static final String CHARACTER_BASE_FILENAME = "CharacterBase";
+    private static final String ID = "id";
+    private static final String ACTOR_ID = "actor_id";
+    private static final String NEXT_ID = "next_id";
+    private static final String DIALOG = "dialog";
+    private static final String DIALOG_CAPTION = "caption";
+    private static final String DIALOG_IS_FAST = "fast";
+    private static final String REPLIC = "replic";
 
-			Element root = doc.getDocumentElement();
-			
-			root.normalize();
-			
-			parseXml(root);
+    Map<Integer, Dialog> dialogs;
+    private String characterBaseFilename;
 
-		} catch (IOException e) {
-			Log.w(MainActivity.DEBUG_ID, "DialogBase IO:" + e);
+    public DialogBase(String filename) {
+        dialogs = new HashMap<Integer, Dialog>();
+        try {
+            InputStream is = MainActivity.Me.getAssets().open(MainActivity.LEVELS_FOLDER + filename);
 
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+            Document doc = docBuilder.parse(is);
+            Element root = doc.getDocumentElement();
+            root.normalize();
+            parseXml(root);
 
-		} catch (SAXException e) {
-			e.printStackTrace();
+        } catch (IOException e) {
+            Log.w(MainActivity.DEBUG_ID, "DialogBase IO:" + e);
 
-		}
-		
-		Log.i(MainActivity.DEBUG_ID, " ");
-		Log.i(MainActivity.DEBUG_ID, "Dialog base " + filename + " loaded and contain:");
-		for (final Map.Entry<Integer, Dialog> entry : dialogs.entrySet()) {
-			Log.i(MainActivity.DEBUG_ID, "  ID: "+ entry.getKey() + " " + entry.getValue());
-		}
-	}
-	
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
 
-	
-	private void parseXml(Element rootElement) {
-		
-		characterBaseFilename = Level.getString(CHARACTER_BASE_FILENAME, rootElement);
-		
-		NodeList items = rootElement.getElementsByTagName(DIALOG);
+        } catch (SAXException e) {
+            e.printStackTrace();
 
-		for (int i = 0; i < items.getLength(); i++) {
+        }
 
-			Element element = (Element) items.item(i);
-			
-			int id = Integer.parseInt(element.getAttribute(ID));
-			
-			Dialog dialog = new Dialog();
-			dialog.setCaption(element.getAttribute(DIALOG_CAPTION));
-			dialog.setFastMessage(Boolean.parseBoolean(element.getAttribute(DIALOG_IS_FAST)));
-			
-			parseReplics(dialog, element);
+        Log.i(MainActivity.DEBUG_ID, " ");
+        Log.i(MainActivity.DEBUG_ID, "Dialog base " + filename + " loaded and contain:");
+        for (final Map.Entry<Integer, Dialog> entry : dialogs.entrySet()) {
+            Log.i(MainActivity.DEBUG_ID, "  ID: " + entry.getKey() + " " + entry.getValue());
+        }
+    }
 
-			dialogs.put(id, dialog);
-		}
-		
+    private void parseXml(Element rootElement) {
 
-	}
-	
-	private void parseReplics(Dialog dialog, Element rootElement) {
-		
-		NodeList items = rootElement.getElementsByTagName(REPLIC);
-		
-		for (int i = 0; i < items.getLength(); i++) {
+        characterBaseFilename = Level.getString(CHARACTER_BASE_FILENAME, rootElement);
 
-			Element element = (Element) items.item(i);
-			
-			int id = Integer.parseInt(element.getAttribute(ID));
-			
-			Replic replic = new Replic();
-			replic.setActorId(Integer.parseInt(element.getAttribute(ACTOR_ID)));
-			replic.setNextId(Integer.parseInt(element.getAttribute(NEXT_ID)));
-			replic.setText(element.getTextContent());
+        NodeList items = rootElement.getElementsByTagName(DIALOG);
 
-			dialog.putReplic(id, replic);
-		}
-	}
+        for (int i = 0; i < items.getLength(); i++) {
 
+            Element element = (Element) items.item(i);
 
-	public String getCharacterBaseFilename() {
-		return characterBaseFilename;
-	}
-	
+            int id = Integer.parseInt(element.getAttribute(ID));
+
+            Dialog dialog = new Dialog();
+            dialog.setCaption(element.getAttribute(DIALOG_CAPTION));
+            dialog.setFastMessage(Boolean.parseBoolean(element.getAttribute(DIALOG_IS_FAST)));
+
+            parseReplics(dialog, element);
+
+            dialogs.put(id, dialog);
+        }
+
+    }
+
+    private void parseReplics(Dialog dialog, Element rootElement) {
+
+        NodeList items = rootElement.getElementsByTagName(REPLIC);
+
+        for (int i = 0; i < items.getLength(); i++) {
+
+            Element element = (Element) items.item(i);
+
+            int id = Integer.parseInt(element.getAttribute(ID));
+
+            Replic replic = new Replic();
+            replic.setActorId(Integer.parseInt(element.getAttribute(ACTOR_ID)));
+            replic.setNextId(Integer.parseInt(element.getAttribute(NEXT_ID)));
+            replic.setText(element.getTextContent());
+
+            dialog.putReplic(id, replic);
+        }
+    }
+
+    public String getCharacterBaseFilename() {
+        return characterBaseFilename;
+    }
+
 }
